50560fc6e5f4a86a4c1e213ed556fc3f52d72680
7079 <rahul@sociusus.com>
2022-07-01 12:29:57 +0530

    [IMP][15.0] Added references

--- pre_commit_hooks/check_branch.py
+++ pre_commit_hooks/check_branch.py
@@ -7,23 +7,32 @@ from git import Git
 
 
 def check_up_to_date(mis_match):
-
     directory = os.getcwd()
     repo = Repo(directory)
     for data in repo.remote().fetch("--dry-run"):
+        # Reference: https://gitpython.readthedocs.io/en/stable/reference.html
+        # Possible values for data.flags
+        # > ERROR= 128
+        # > FAST_FORWARD= 64
+        # > FORCED_UPDATE= 32
+        # > HEAD_UPTODATE= 4
+        # > NEW_HEAD= 2
+        # > NEW_TAG= 1
+        # > REJECTED= 16
+        # > TAG_UPDATE= 8
         if data.flags != 4 and (data.remote_ref_path).strip() == "13.0":
             mis_match = True
             print(
-                    f'[FD813].'
-                    f'Your local repository is not up'
-                    f'to date with production repository'
-                )
+                f'[FD813].'
+                f'Your local repository is not up'
+                f'to date with production repository'
+            )
     if repo.git.rev_list("..13.0"):
         mis_match = True
         print(
-                    f'[FD813].'
-                    f'Your branch is not up to date with origin/13.0'
-                )
+            f'[FD813].'
+            f'Your branch is not up to date with origin/13.0'
+        )
     return mis_match
 
 
